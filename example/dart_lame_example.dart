b884397e3298a4e6ea9b340dd6e623de4e238e8b
NightOwl <midnightow1@outlook.com>
2022-10-02 20:47:03 +0800

    Initial commit

---
+++ example/dart_lame_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_lame/dart_lame.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_lame.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_lame;
+
+export 'src/dart_lame_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
